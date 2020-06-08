ac8f9a44d84e6b1687f25a47018db634f2324fa6
Rahul Katiyar <rahulkatiyar19955@gmail.com>
2020-06-08 22:10:49 +0530

    Added tests

--- calender_api_client/google_calender.py
+++ calender_api_client/google_calender.py
@@ -2,7 +2,8 @@
 
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
-from datetime import datetime, timedelta
+from datetime import datetime, time
+
 
 class CalenderApi(object):
     """docstring for CalenderApi."""
@@ -16,26 +17,32 @@ class CalenderApi(object):
         self.delegated_credentials = self.credentials.with_subject(self.service_account_name)
         self.service = build('calendar', 'v3', credentials=self.delegated_credentials)
 
-    def getEvent(self,date):
+    # this method is used to fetch event for a particular date
+    def getEvents(self, date_argument):
         eventList = []
         try:
             page_token = None
-            timeMin = date.isoformat()
-            timeMax = (date + timedelta(days=1)).isoformat()
+            startTime = time(hour=0, minute=0, second=0)
+            endTime = time(hour=23, minute=59, second=59)
+            today_beginning = datetime.combine(date_argument, startTime).isoformat() + 'Z'
+            today_ending = datetime.combine(date_argument, endTime).isoformat() + 'Z'
             while True:
                 events = self.service.events().list(calendarId=self.service_account_name,
                                                     pageToken=page_token,
-                                                    timeMin = timeMin,
-                                                    timeMax= timeMax).execute()
+                                                    timeMin=today_beginning,
+                                                    timeMax=today_ending).execute()
                 for event in events['items']:
-                    print(event['summary'])
+                    eventList.append(event)
                 page_token = events.get('nextPageToken')
                 if not page_token:
                     break
         except:
             print('The credentials have been revoked or expired, please re-run'
                   'the application to re-authorize.')
+        finally:
+            return eventList
 
+    # get all events on a calendar
     def getAllEvent(self):
         eventList = []
         try:
@@ -45,7 +52,6 @@ class CalenderApi(object):
                                                     pageToken=page_token).execute()
                 for event in events['items']:
                     eventList.append(event)
-                    print(event)
                 page_token = events.get('nextPageToken')
                 if not page_token:
                     break
@@ -54,7 +60,8 @@ class CalenderApi(object):
             print('The credentials have been revoked or expired, please re-run'
                   'the application to re-authorize.')
 
-    def createEvent(self,bodyContent):
+    # create a event with the information provided in the bodycontent
+    def createEvent(self, bodyContent):
         """Function to create event in the calender"""
         # sample body example
         # body = {"summary": 'Activity Number',
@@ -71,6 +78,7 @@ class CalenderApi(object):
         event = self.service.events().insert(calendarId=self.service_account_name, body=bodyContent).execute()
         return event
 
+    # get a list of calendars
     def getCalenderList(self):
         try:
             calendar = self.service.calendars().get(calendarId='primary').execute()
@@ -80,4 +88,16 @@ class CalenderApi(object):
             print('The credentials have been revoked or expired, please re-run'
                   'the application to re-authorize.')
 
-
+    # this method is used to give access to a email address,
+    # so that using this id we can manipulate event using
+    # browser
+    def giveAccess(self,emailAddress):
+        rule = {
+            'scope': {
+                'type': "user",
+                'value': emailAddress,
+            },
+            'role': 'owner'
+        }
+        created_rule = self.service.acl().insert(calendarId='primary', body=rule).execute()
+        return created_rule['id']
--- calender_api_client/test.py
+++ calender_api_client/test.py
@@ -1,17 +1,18 @@
 from google_calender import CalenderApi
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta, date, time
 import json
 
+# initializing the Calendar API object
 apiObj = CalenderApi()
-# apiObj.getEvent()
-# apiObj.getCalenderList()
+
+# today date
 d = datetime.utcnow().date()
-tomorrow = datetime(d.year, d.month, d.day, 10)+timedelta(days=1)
+
+# tommorow date
+tomorrow = datetime(d.year, d.month, d.day, 10) + timedelta(days=1)
+
 start = tomorrow.isoformat()
 end = (tomorrow + timedelta(hours=1)).isoformat()
-desciptionJson = {'Type': 'Individual', 'IndividualName': '', 'TherapistName': '', 'Location': 'Physical therapy room', 'Element': 'Stretcher', 'Notification': False}
-body={'summary': 'Activity 2', 'description': json.dumps(desciptionJson), 'start': {'dateTime': '2020-06-06T00:00:00Z'}, 'end': {'dateTime': '2020-06-06T05:00:00Z'}}
-
 
 # sample object example
 #     { "Type": "Individual",
@@ -26,16 +27,52 @@ body={'summary': 'Activity 2', 'description': json.dumps(desciptionJson), 'start
 #         "start": {"dateTime": start, "timeZone": 'Asia/Kolkata'},
 #         "end": {"dateTime": end, "timeZone": 'Asia/Kolkata'},
 #         }
-eventList = apiObj.getAllEvent()
-for event in eventList:
-    try:
-        print(event["description"])
-        jsonObj = json.loads(event["description"])
-        print(jsonObj["Type"])
-    except:
-        pass
 
-# apiObj.createEvent(bodyContent=body)
-# apiObj.getAllEvent()
+desciptionJson = {'Type': 'Individual', 'IndividualName': '', 'TherapistName': '', 'Location': 'Physical therapy room',
+                  'Element': 'Stretcher', 'Notification': False}
+body = {'summary': 'Activity test', 'description': json.dumps(desciptionJson),
+        'start': {'dateTime': '2020-06-06T00:00:00Z'}, 'end': {'dateTime': '2020-06-06T05:00:00Z'}}
 
-# apiObj.getAllEvent()
+
+def Call_CreateEvent():
+    apiObj.createEvent(bodyContent=body)
+
+
+# get all events for a particular day
+def Call_getEvents():
+    date_obj = date.today()
+    date_obj = datetime(date_obj.year, date_obj.month, date_obj.day, 10) + timedelta(days=1)
+    # getting events for tommorow
+    eventList = apiObj.getEvents(date_obj)
+    for event in eventList:
+        try:
+            print(event["summary"])
+            jsonObj = json.loads(event["description"])
+        except:
+            pass
+
+
+# get all event in the calendar
+def Call_getAllEvent():
+    eventList = apiObj.getAllEvent()
+    for event in eventList:
+        try:
+            print(event["summary"])
+            jsonObj = json.loads(event["description"])
+        except:
+            pass
+
+
+# insert a email address to authorize to view the calendar
+# emailAddress should be a string
+def Call_giveAccess(emailAddress):
+    print(apiObj.giveAccess(emailAddress=emailAddress))
+
+
+def main():
+    Call_CreateEvent()
+    Call_getAllEvent()
+
+
+if __name__ == "__main__":
+    main()
